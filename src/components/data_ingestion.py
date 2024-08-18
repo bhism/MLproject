b0cb35ddfedbf35144dc0ee00bf6fb37f887901b
Chetan Chhogaram Parmar <ChetanP@quinnox.com>
2024-08-18 19:19:57 +0530

    data injestion and data transformation completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,11 @@ from datetime import datetime
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transfomation import DataTransformation
+from src.components.data_transfomation import DataTransformationConfig
+
+# from src.components.model_trainer import ModelTrainerConfig
+# from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     # Generate a timestamp to create a unique versioned folder for each run
@@ -18,6 +23,7 @@ class DataIngestionConfig:
     train_data_path: str = os.path.join(versioned_folder, "train.csv")
     test_data_path: str = os.path.join(versioned_folder, "test.csv")
     raw_data_path: str = os.path.join(versioned_folder, "data.csv")
+
 
 class DataIngestion:
     def __init__(self):
@@ -53,5 +59,11 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    # modeltrainer=ModelTrainer()
+    # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
